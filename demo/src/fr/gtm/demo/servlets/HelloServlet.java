f42d5bec31d6427adfc3168b57245f856f064ab0
erwansbd <erwansbd@gmail.com>
2019-10-16 11:09:21 +0200

    Ajout Servlet

---
+++ demo/src/fr/gtm/demo/servlets/HelloServlet.java
@@ -0,0 +1,44 @@
+package fr.gtm.demo.servlets;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Writer out = response.getWriter();
+		String nom = request.getParameter("nom");
+		if(nom==null) {
+			nom = "Jean-Pierre";
+		}
+		out.append("<html><head><title>Hello ")
+			.append(nom)
+			.append("</title></head>")
+			.append("<body><h2>Bonjour ")
+			.append(nom)
+			.append("</h2></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
